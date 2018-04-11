f34a5ae7a3930a0ebae6ab7da16a67068d116fea
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:41:14 +0800

    Added relationships to SharePayment

--- app/SharePayment.php
+++ app/SharePayment.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SharePayment extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function member() {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function receivedBy() {
+        return $this->belongsTo(Admin::class);
+    }
 }
