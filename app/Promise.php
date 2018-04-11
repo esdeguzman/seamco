81117c40623b01143983633e98821500201dafc4
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:50:15 +0800

    Added relationships to Promise

--- app/Promise.php
+++ app/Promise.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Promise extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function promissoryNote() {
+        return $this->belongsTo(PromissoryNote::class);
+    }
+
+    public function payment() {
+        return $this->hasOne(LoanPayment::class);
+    }
 }
