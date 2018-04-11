198f869eb3cdc1f48943043055a489f2c669cfb9
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:33:53 +0800

    Added relationships

--- app/Loan.php
+++ app/Loan.php
@@ -3,8 +3,31 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Loan extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function member() {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function comaker() {
+        return $this->hasOne(Comaker::class);
+    }
+
+    public function creditEvaluation() {
+        return $this->belongsTo(CreditEvaluation::class);
+    }
+
+    public function payments() {
+        return $this->hasMany(LoanPayment::class);
+    }
+
+    public function promissoryNote() {
+        return $this->hasOne(PromissoryNote::class);
+    }
 }
