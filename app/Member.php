96bfb88b60e955c3c9941198560c3b2001f070bd
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:25:59 +0800

    Added relationships

--- app/Member.php
+++ app/Member.php
@@ -3,39 +3,46 @@
 namespace App;
 
 use App\Notifications\MemberResetPassword;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Member extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
+    protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * Send the password reset notification.
-     *
-     * @param  string  $token
-     * @return void
-     */
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new MemberResetPassword($token));
+    }
+
+    public function loans() {
+        return $this->hasMany(Loan::class);
+    }
+
+    public function comakers() {
+        return $this->hasMany(Comaker::class);
+    }
+
+    public function loanPayments() {
+        return $this->hasMany(LoanPayment::class);
+    }
+
+    public function sharePayments() {
+        return $this->hasMany(SharePayment::class);
+    }
+
+    public function promissoryNotes() {
+        return $this->hasMany(PromissoryNote::class);
+    }
+
+    public function creditEvaluations() {
+        return $this->hasMany(CreditEvaluation::class);
     }
 }
