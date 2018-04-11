06a85668c98065b0198a0b3074c21a7408cbde6c
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:47:45 +0800

    Added relationships to PromissoryNote

--- app/PromissoryNote.php
+++ app/PromissoryNote.php
@@ -3,8 +3,27 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PromissoryNote extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function member() {
+        return $this->belongsTo(Member::class);
+    }
+
+    public function loan() {
+        return $this->belongsTo(Loan::class);
+    }
+
+    public function promises() {
+        return $this->hasMany(Promise::class);
+    }
+
+    public function creditEvaluation() {
+        return $this->belongsTo(CreditEvaluation::class);
+    }
 }
