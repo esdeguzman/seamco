21887e37cdaa0209bdb872d904fb3faca56d486a
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:52:37 +0800

    Added relationships to CreditEvaluation

--- app/CreditEvaluation.php
+++ app/CreditEvaluation.php
@@ -3,8 +3,23 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CreditEvaluation extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function loan() {
+        return $this->hasOne(Loan::class);
+    }
+
+    public function promissoryNote() {
+        return $this->hasOne(PromissoryNote::class);
+    }
+
+    public function member() {
+        return $this->belongsTo(Member::class);
+    }
 }
