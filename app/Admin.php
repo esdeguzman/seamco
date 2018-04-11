a782c1b489effa21488af620968ceaa5c5fd66a5
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:15:31 +0800

    Added relationships

--- app/Admin.php
+++ app/Admin.php
@@ -3,38 +3,32 @@
 namespace App;
 
 use App\Notifications\AdminResetPassword;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Admin extends Authenticatable
 {
-    use Notifiable;
-    use SoftDelete;
+    use Notifiable, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * Send the password reset notification.
-     *
-     * @param  string  $token
-     * @return void
-     */
     public function sendPasswordResetNotification($token)
     {
         $this->notify(new AdminResetPassword($token));
+    }
+
+    // TODO: make approvedMembers(), deniedMembers(), approvedLoans() and deniedLoans()
+
+    public function acceptedLoanPayments() {
+        return $this->hasMany(LoanPayment::class);
+    }
+
+    public function acceptedSharePayments() {
+        return $this->hasMany(SharePayment::class);
     }
 }
