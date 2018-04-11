0b3d61d7a7586aa2fb0080b48a32a129a74dd11d
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-11 15:30:06 +0800

    Added relationships

--- app/Comaker.php
+++ app/Comaker.php
@@ -3,8 +3,19 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comaker extends Model
 {
-    //
+    use SoftDeletes;
+
+    protected $guarded = [];
+
+    public function members() {
+        return $this->belongsToMany(Member::class);
+    }
+
+    public function loans() {
+        return $this->hasMany(Loan::class);
+    }
 }
