a5b59b37509ad1f7b497e7bc27ce880e0a6de390
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-12 14:43:30 +0800

    Added columns to loans table

--- database/migrations/2018_04_11_070415_create_loans_table.php
+++ database/migrations/2018_04_11_070415_create_loans_table.php
@@ -15,6 +15,24 @@ class CreateLoansTable extends Migration
     {
         Schema::create('loans', function (Blueprint $table) {
             $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('credit_evaluation_id');
+            $table->tinyInteger('status')->default('0');
+            $table->decimal('regular', 20, 2)->default('0.0');
+            $table->decimal('short_term', 20, 2)->default('0.0');
+            $table->decimal('pre_joining', 20, 2)->default('0.0');
+            $table->decimal('productive', 20, 2)->default('0.0');
+            $table->decimal('special', 20, 2)->default('0.0');
+            $table->decimal('appliance', 20, 2)->default('0.0');
+            $table->decimal('petty_cash', 20, 2)->default('0.0');
+            $table->tinyInteger('payment_terms');
+            $table->string('company_contact_number');
+            $table->string('monthly_income');
+            $table->string('take_home_pay');
+            $table->string('sss_gsis');
+            $table->string('residence_telephone_number');
+            $table->unsignedInteger('total_amount');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
