f471f543c4a81de459c9028465a8d472ba20a435
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-13 10:52:01 +0800

    Fixed username field name attribute

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -14,7 +14,7 @@
                             <label for="username" class="col-md-4 control-label">Username</label>
 
                             <div class="col-md-6">
-                                <input id="username" type="username" class="form-control" name="text" value="{{ old('username') }}" autofocus>
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" autofocus>
 
                                 @if ($errors->has('username'))
                                     <span class="help-block">
