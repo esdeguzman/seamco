dcc2c52f94304abea29394a4e7622e163b4743a7
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-13 10:50:21 +0800

    Added admin middleware

--- app/Http/Controllers/AdminsController.php
+++ app/Http/Controllers/AdminsController.php
@@ -6,6 +6,11 @@ use Illuminate\Http\Request;
 
 class AdminsController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('admin', ['except' => 'logout']);
+    }
+
     public function dashboard() {
         return view('admin.layout.dashboard');
     }
