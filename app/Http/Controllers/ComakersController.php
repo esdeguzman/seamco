cb565c883d68871ddb6988c7dc9c67bedd21537f
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-16 16:52:33 +0800

    Added update function for loan request response

--- app/Http/Controllers/ComakersController.php
+++ app/Http/Controllers/ComakersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Comaker;
 use Illuminate\Http\Request;
 
 class ComakersController extends Controller
@@ -9,5 +10,27 @@ class ComakersController extends Controller
     public function __construct()
     {
         $this->middleware('member', ['except' => 'logout']);
+    }
+
+    public function update(Comaker $comaker, Request $request) {
+        if($request->response == 'APPROVED COMAKER REQUEST') {
+            $comaker->status = 1;
+
+            $request->session()->flash('success', 'You have successfully approved this loan\'s comaker request!');
+        }
+    else if($request->response == 'DENY COMAKER REQUEST') {
+            $comaker->status = 0;
+
+            $comaker->loan->creditEvaluation->delete();
+            $loan = $comaker->loan;
+            $loan->status = 0;
+            $loan->save();
+
+            $request->session()->flash('success', 'You have successfully denied this loan\'s comaker request!, loan request has been closed!');
+        }
+
+        $comaker->save();
+
+        return back();
     }
 }
