f91af2c77777fba8158579d9cdad4ea084fd43a2
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-16 16:39:34 +0800

    Added function contruct with member middleware setup

--- app/Http/Controllers/ComakersController.php
+++ app/Http/Controllers/ComakersController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class ComakersController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('member', ['except' => 'logout']);
+    }
 }
