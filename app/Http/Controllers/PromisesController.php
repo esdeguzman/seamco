7f3847048ab897f8f9d83158ae117b264d77a200
esdeguzman <deguzman.esmeraldo@gmail.com>
2018-04-17 09:33:49 +0800

    Added member middleware

--- app/Http/Controllers/PromisesController.php
+++ app/Http/Controllers/PromisesController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class PromisesController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('member', ['except' => 'logout']);
+    }
 }
