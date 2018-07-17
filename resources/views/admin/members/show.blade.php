4b74e3793a2114a42208282cc62614f141a85753
Esmeraldo Barios de Guzman Jr <deguzman.esmeraldo@gmail.com>
2018-07-17 16:35:58 +0800

    Add member update route to actually update member's info

--- resources/views/admin/members/show.blade.php
+++ resources/views/admin/members/show.blade.php
@@ -29,8 +29,8 @@
                                     </ul>
                                     <div id="myTabContent" class="tab-content">
                                         <div role="tabpanel" class="tab-pane fade active in" id="tab_content1" aria-labelledby="home-tab">
-                                            <form class="form-horizontal form-label-left">
-
+                                            <form class="form-horizontal form-label-left" action="{{ route('admins.update-member-info', $member->id) }}" method="post">
+                                                {{ csrf_field() }} {{ method_field('put') }}
                                                 <div class="col-md-12 col-sm-12 col-xs-12 form-group">
                                                     <input type="text" class="form-control has-feedback-left" placeholder="First Name" name="full_name" value="{{ $member->full_name }}" />
                                                     <span class="fa fa-user form-control-feedback left" aria-hidden="true"></span>
