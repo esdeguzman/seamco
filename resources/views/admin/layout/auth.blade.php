72b9eeb12da564d35097c63180f532ff1d465ace
Esmeraldo <deguzman.esmeraldo@gmail.com>
2019-10-05 20:17:07 +0800

    =Google AdSense removal

--- resources/views/admin/layout/auth.blade.php
+++ resources/views/admin/layout/auth.blade.php
@@ -20,12 +20,6 @@
         ]); ?>
     </script>
     <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-         (adsbygoogle = window.adsbygoogle || []).push({
-              google_ad_client: "ca-pub-6429484135715885",
-              enable_page_level_ads: true
-         });
-    </script>
 </head>
 <body>
     <nav class="navbar navbar-default navbar-static-top">
